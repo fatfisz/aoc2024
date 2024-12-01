6fdaf3aab4fc7fa7ea34b51a1163a56eb82227c2
fatfisz <fatfisz@gmail.com>
2024-12-01 23:33:15 +0100

    Day 1

---
+++ task_1_1/src/main.rs
@@ -0,0 +1,26 @@
+// #![allow(warnings)]
+
+use std::{fs::read_to_string, iter::zip};
+
+fn main() {
+    let input = read_to_string("input.txt").unwrap();
+    let mut first_list = Vec::<i32>::new();
+    let mut second_list = Vec::<i32>::new();
+
+    for line in input.split('\n') {
+        let mut numbers = line.split_whitespace();
+        first_list.push(numbers.next().unwrap().parse().unwrap());
+        second_list.push(numbers.next().unwrap().parse().unwrap());
+    }
+
+    first_list.sort();
+    second_list.sort();
+
+    let mut distance_sum = 0;
+
+    for (first, second) in zip(&first_list, &second_list) {
+        distance_sum += first.abs_diff(*second);
+    }
+
+    println!("{distance_sum}");
+}
---
+++ task_1_2/src/main.rs
@@ -0,0 +1,24 @@
+// #![allow(warnings)]
+
+use std::{collections::HashMap, fs::read_to_string};
+
+fn main() {
+    let input = read_to_string("input.txt").unwrap();
+    let mut number_list = Vec::<i32>::new();
+    let mut counts = HashMap::<i32, i32>::new();
+
+    for line in input.split('\n') {
+        let mut numbers = line.split_whitespace();
+        number_list.push(numbers.next().unwrap().parse().unwrap());
+        let second = numbers.next().unwrap().parse().unwrap();
+        counts.insert(second, counts.get(&second).unwrap_or(&0) + 1);
+    }
+
+    let mut similarity_score = 0;
+
+    for number in number_list {
+        similarity_score += counts.get(&number).unwrap_or(&0) * number;
+    }
+
+    println!("{similarity_score}");
+}
